b37862396345e3975512e888dbc72d7c1f0e815a
Remington <remingtonnberg@gmail.com>
2020-10-28 14:27:50 -0500

    display products

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -3,4 +3,19 @@ class Api::ProductsController < ApplicationController
     @products = Product.all
     render "all.json.jb"
   end
+
+  def quinn
+    @product = Product.first
+    render "quinn.json.jb"
+  end
+
+  def culkin
+    @product = Product.second
+    render "culkin.json.jb"
+  end
+
+  def jackson
+    @product = Product.third
+    render "jackson.json.jb"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     get "/all_products" => "products#display_products"
+    get "/Quinn" => "products#quinn"
+    get "/culkin" => "products#culkin"
+    get "/jackson" => "products#jackson"
   end
 end
