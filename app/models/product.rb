07072d44164affb7062fbcbde5493355420cdbb6
Remington <remingtonnberg@gmail.com>
2020-11-04 11:51:19 -0600

    adds validates to model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
+  validates :price, presence: true, numericality: { greater_than: 0 }
+  validates :description, length: { minimum: 10, maximum: 500 }
+
   def is_discounted?
     price <= 10
   end
