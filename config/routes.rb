c23624d93e301f61b20b4b2af488fc7f258f8012
Remington <remingtonnberg@gmail.com>
2020-11-11 12:39:59 -0600

    adds order controller

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
     post "/sessions" => "sessions#create"
 
     post "orders" => "orders#create"
+    get "orders" => "orders#index"
+    get "orders/:id" => "orders#show"
 
     get "/products" => "products#index"
     get "/products/:id" => "products#show"
